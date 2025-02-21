e00fa59c6103ca42a13465580523eab88dcb3738
diabatesalia <diabatesalia0@gmail.com>
2025-02-21 22:21:34 +0000

    la bas de donné

--- commande/models.py
+++ commande/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Commande(models.Model):
+    STATUS=(('en instance', 'en instance'),
+            ('nom livré', 'nom livré'),
+            ('livré', 'livré'))
+    #client=
+    #produit=
+    status=models.CharField(max_length=200, null=True, choices=STATUS)
+    date_creation=models.DateTimeField(auto_now_add=True,null=True)
--- produit/models.py
+++ produit/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Produit(models.Model):
+    nom=models.CharField(max_length=200, null=True)
+    prix=models.FloatField(null=True)
+    date_creation=models.DateTimeField(auto_now_add=True,null=True)
+
+    def __str__(self):
+      return self.nom
